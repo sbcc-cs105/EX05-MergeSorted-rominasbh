1e3f313babbd8318effe24816d55b62e6f40d24c
rominasbh <60419005+rominasbh@users.noreply.github.com>
2020-04-23 16:07:43 -0700

    Submitting code for a good grade after 3 days of rewriting the same code 20 times:)

--- src/main/java/edu/sbcc/cs105/MergeSorted.java
+++ src/main/java/edu/sbcc/cs105/MergeSorted.java
@@ -2,11 +2,11 @@
  * CS 105 Theory & Practice
  * CRN: 62498
  * Assignment: MergeSorted
- * 
+ *
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
- * 
- * @author Allan Knight
+ *
+ * @author Romina Sobhani, rsobhani@pipeline.sbcc.edu
  */
 package edu.sbcc.cs105;
 
@@ -19,20 +19,77 @@ import java.util.Arrays;
  */
 public class MergeSorted {
 
+
 	public static ArrayList<Integer> mergeSorted(ArrayList<Integer> a, ArrayList<Integer> b) {
-		// put your code for this assignment here
-	}
+		ArrayList<Integer> sort = new ArrayList<>();
+		int indexA=0;
+		int indexB=0;
 
-	public static void main(String args[]) {
-		ArrayList<Integer> x = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 5, 7));
-		ArrayList<Integer> y = new ArrayList<>(Arrays.asList(2, 2, 4, 5, 8));
 
-		ArrayList<Integer> sorted = mergeSorted(y, x);
-		for (int val: sorted) {
-			System.out.print(val + " ");
+		/*for (int element:a){
+			if(element<b.get(indexB)) {
+				sort.add(element);
+				indexA++;
+			}
+			else if(element>=b.get(indexB)) {
+				sort.add(b.get(indexB));
+				indexB++;
+			}*/
+		int sizeA=a.size();
+		int sizeB=b.size();
+		int leftA=a.size();
+		int leftB=b.size();
+		int total=0;
+
+
+		while (leftA!=0 && leftB!=0){
+
+			//int A= a.get(indexA);
+			//int B= b.get(indexB);
+			if (a.get(indexA) <b.get(indexB) ) {
+				sort.add(a.get(indexA));
+				indexA++;
+				leftA--;}
+
+			else if (b.get(indexB) < a.get(indexA)) {
+				sort.add(b.get(indexB));
+				indexB++;
+				leftB--;
+			}
+
+			else if (b.get(indexB) == a.get(indexA)) {
+				if (leftA >= leftB) {
+					sort.add((a.get(indexA)));
+					indexA++;
+					leftA--;
+				}
+
+				} else {
+					sort.add(b.get(indexB));
+					indexB++;
+					leftB--;
+					}
+		if (leftA!=0){
+			for (int i=indexA;i<sizeA;i++ ){
+				sort.add(a.get(i));
+				leftA--;
+			}
 		}
-		System.out.println();
+		if(leftB!=0){
+			for (int i=indexB; i<sizeB; i++){
+				sort.add(b.get(i));
+				leftB--;}
+			}}
+		return sort;
+			}
 
-		// you can put more code here to do more testing
-	}
-}
+			public static void main(String args[]) {
+			ArrayList<Integer> x = new ArrayList<>(Arrays.asList(1,2,3,4,5,5,7));
+			ArrayList<Integer> y = new ArrayList<>(Arrays.asList(2,2,4,5,8));
+			ArrayList<Integer> sorted = mergeSorted(x,y);
+				for (int val: sorted){
+					System.out.print(val);
+					System.out.print(" ");
+		}
+	}}
+
