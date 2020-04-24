0b8eb1b9926fa0a0a487f03b3d3d609ae126fc8d
rominasbh <60419005+rominasbh@users.noreply.github.com>
2020-04-23 19:53:13 -0700

    submitting code for a good grade

--- src/main/java/edu/sbcc/cs105/MergeSorted.java
+++ src/main/java/edu/sbcc/cs105/MergeSorted.java
@@ -31,12 +31,6 @@ public class MergeSorted {
 		int sizeB=b.size();
 		int leftA=a.size();
 		int leftB=b.size();
-		int total=0;
-		/*if (sizeA==0 && sizeB==0){
-			sort.add(0);
-
-		}
-		else{*/
 
 
 		while (leftA!=0 && leftB!=0) {
@@ -56,11 +50,11 @@ public class MergeSorted {
 					leftA--;
 				}
 
-			} else {
+			 else {
 				sort.add(b.get(indexB));
 				indexB++;
 				leftB--;
-			}
+			}}
 		}
 		if (leftA!=0){
 			for (int i=indexA;i<sizeA;i++ ){
@@ -77,37 +71,6 @@ public class MergeSorted {
 			}
 
 			public static void main(String args[]) {
-			ArrayList<Integer> x = new ArrayList<>(Arrays.asList(1,2,4,5,5,7));
-			ArrayList<Integer> y = new ArrayList<>(Arrays.asList(2,2,4,5,8));
-			ArrayList<Integer> sorted = mergeSorted(x,y);
-				for (int val: sorted){
-					System.out.print(val);
-					System.out.print(" ");}
-				System.out.println();
-					 ArrayList<Integer> z = new ArrayList<>(Arrays.asList(0));
-						ArrayList<Integer> f = new ArrayList<>();
-						ArrayList<Integer> sorted1 = mergeSorted(z,f);
-						for (int val1: sorted1){
-							System.out.print(val1);
-							System.out.print(" ");
-						}
-				System.out.println();
-						ArrayList<Integer> n = new ArrayList<>(Arrays.asList(0));
-							ArrayList<Integer> m = new ArrayList<>(Arrays.asList(1));
-							ArrayList<Integer> sorted2 = mergeSorted(n,m);
-							for (int val2: sorted2){
-								System.out.print(val2);
-								System.out.print(" ");
-								}
-				System.out.println();
-						 ArrayList<Integer> l = new ArrayList<>(Arrays.asList(1,2,4,5,5,7));
-							ArrayList<Integer> s = new ArrayList<>();
-							ArrayList<Integer> sorted3 = mergeSorted(l,s);
-							for (int val3: sorted3){
-								System.out.print(val3);
-								System.out.print(" ");
-								}
-				System.out.println();
 				ArrayList<Integer> h = new ArrayList<>();
 				ArrayList<Integer> b = new ArrayList<>();
 				ArrayList<Integer> sorted4 = mergeSorted(h,b);
@@ -115,6 +78,47 @@ public class MergeSorted {
 					System.out.print(val4);
 					System.out.print(" ");}
 				System.out.println();
+
+				ArrayList<Integer> z = new ArrayList<>(Arrays.asList(0));
+				ArrayList<Integer> f = new ArrayList<>();
+				ArrayList<Integer> sorted1 = mergeSorted(z,f);
+				for (int val1: sorted1){
+					System.out.print(val1);
+					System.out.print(" ");
+				}
+				System.out.println();
+
+				ArrayList<Integer> n = new ArrayList<>(Arrays.asList(0));
+				ArrayList<Integer> m = new ArrayList<>(Arrays.asList(1));
+				ArrayList<Integer> sorted2 = mergeSorted(n,m);
+				for (int val2: sorted2){
+					System.out.print(val2);
+					System.out.print(" ");
+				}
+				System.out.println();
+
+
+			ArrayList<Integer> k = new ArrayList<>(Arrays.asList(1,2,4,5,5,7));
+			ArrayList<Integer> q = new ArrayList<>(Arrays.asList(2,2,4,5,8));
+			ArrayList<Integer> sorted = mergeSorted(k,q);
+				for (int val: sorted){
+					System.out.print(val);
+					System.out.print(" ");}
+				System.out.println();
+
+
+				ArrayList<Integer> l = new ArrayList<>(Arrays.asList(1,2,4,5,5,7));
+				ArrayList<Integer> s = new ArrayList<>();
+				ArrayList<Integer> sorted3 = mergeSorted(l,s);
+				for (int val3: sorted3){
+					System.out.print(val3);
+					System.out.print(" ");
+				}
+				System.out.println();
+
+
+
+
 
 
 
